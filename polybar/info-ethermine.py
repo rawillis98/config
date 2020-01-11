1bf8c2aa9c025f064e73a54243d02a7f912cb9e4
Alex Willis <ricwillis98@gmail.com>
2020-01-11 09:39:30 -0600

    Changed ethermine to HTTP

--- polybar/info-ethermine.py
+++ polybar/info-ethermine.py
@@ -4,7 +4,7 @@ import requests
 
 miner = "857fa14a9c7de4436ca08002a4284d5f38b22476"
 
-data = requests.get(f"https://api.ethermine.org/miner/{miner}/dashboard").json()['data']['statistics']
+data = requests.get(f"http://api.ethermine.org/miner/{miner}/dashboard").json()['data']['statistics']
 data = data[-1]
 current_hash_rate = data["currentHashrate"]
 currentMHs = round(current_hash_rate/1e6, 1)
